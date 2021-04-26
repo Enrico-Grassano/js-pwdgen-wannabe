86a34c36e9982854242cd0f01280ab6f7fddf576
Enrico-Grassano <grassano.enrico1996@gmail.com>
2021-04-26 17:57:58 +0200

    js-pwdgen-wannabe completed

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+var nomeUtente = prompt("Come ti chiami?");
+console.log(nomeUtente);
+
+var cognomeUtente = prompt("Qual'è il tuo cognome?");
+console.log(cognomeUtente);
+
+var colorePreferito = prompt("Qual'è il tuo colore preferito?");
+console.log(colorePreferito);
+
+var number = "21"
+
+document.getElementById ("password").innerHTML = nomeUtente + cognomeUtente + colorePreferito + number;
